9c097ad6ebdf0bdaa674697028968fb99b08a91b
Gustavo Pereira <gustavopereirasantos@hotmail.com>
2022-08-19 12:25:49 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <Box>
       <Box bg="gray.100" w="100%" p={4}>
         <Heading as="h2" size="3xl" noOfLines={1}>
-          Aprenda programação direto ao ponto
+          Aprenda programação direto ao ponto 100% FREE
         </Heading>
       </Box>
       {/* <h2>Aprenda programação direto ao ponto 100% free</h2>
