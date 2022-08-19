4ecd44baf2701d8620e770437fc816b6069a3f9e
Gustavo Pereira <gustavopereirasantos@hotmail.com>
2022-08-18 23:29:58 -0300

    Autenticação com o Github Funcionando

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import useAuth from "../hooks/useAuth"
+
 export default function Home() {
+
+  const { user, signin } = useAuth();
+  console.log('user', user)
   return (
     <div>
       <h2>Aprenda programação direto ao ponto 100% free</h2>
+
+      <button onClick={() => signin()}>Entrar com github</button>
     </div>
   )
 }
